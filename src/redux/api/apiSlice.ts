84ec6242f79c8bd18e10646b1b338f0b66623f8e
M Rafiul Alam Tonmoy <mrafiul.alam7@gmail.com>
2023-07-28 20:04:44 +0600

    CHANGED: Api

--- src/redux/api/apiSlice.ts
+++ src/redux/api/apiSlice.ts
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const api = createApi({
   reducerPath: "api",
   baseQuery: fetchBaseQuery({
-    baseUrl: "http://localhost:5000/api/v1/careHome",
+    baseUrl: "https://care-home-backend.vercel.app/api/v1/careHome",
   }),
   tagTypes: ["comments"],
   endpoints: () => ({}),
