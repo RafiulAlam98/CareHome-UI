c4a4418e4ada47275f7fc7e575166552c9238f2a
RafiulAlam <mrafiul.alam7@gmail.com>
2023-08-26 15:59:58 +0600

    search and filter regenerated

--- src/components/SearchAndFilter/SearchAndFilter.tsx
+++ src/components/SearchAndFilter/SearchAndFilter.tsx
@@ -1,28 +1,35 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 export default function SearchAndFilter({
-  setSelectedGenre,
-  setSearchText,
+  features,
+  filterableData,
   searchText,
-  selectedGenre,
+  setSearchText,
 }: any) {
+  const selectedField = (event: any) => {
+    console.log(event.target.value);
+    filterableData(event.target.value);
+  };
   return (
     <div className="p-2 text-center">
-      <select
-        className="select select-sm rounded select-info w-1/3 mr-2 "
-        value={selectedGenre}
-        onChange={(e) => setSelectedGenre(e.target.value)}
-      >
-        <option value="">Filter</option>
-        <option value="location">Location</option>
-        <option value="title">Name</option>
-      </select>
       <input
         type="text"
         className="input input-sm rounded  input-primary w-1/3 "
         placeholder="Search by title or name or location"
         value={searchText}
-        onChange={(e) => setSearchText(e.target.value)}
+        onChange={e => setSearchText(e.target.value)}
       />
+      <select
+        name="isAvailable"
+        className="select select-sm rounded select-info w-1/3 mr-2 "
+        onChange={e => selectedField(e)}
+      >
+        <option value="all">All Types of Care Home</option>
+        {features?.map((item: any) => (
+          <option key={item._id} value={item?.title}>
+            {item.title}
+          </option>
+        ))}
+      </select>
     </div>
   );
 }
--- src/pages/Home/FeaturedSection/FeaturedSection.tsx
+++ src/pages/Home/FeaturedSection/FeaturedSection.tsx
@@ -1,14 +1,13 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 
-import { Link } from "react-router-dom";
-import { useGetFeatureQuery } from "../../../redux/features/featureApi";
-import SearchAndFilter from "../../../components/SearchAndFilter/SearchAndFilter";
-import { useState } from "react";
-import Fuse from "fuse.js";
+import { Link } from 'react-router-dom';
+import SearchAndFilter from '../../../components/SearchAndFilter/SearchAndFilter';
+import { useGetFeatureQuery } from '../../../redux/features/featureApi';
+import { useState } from 'react';
 
 export default function FeaturedSection() {
-  const [searchText, setSearchText] = useState("");
-  const [selectedGenre, setSelectedGenre] = useState("");
+  const [searchText, setSearchText] = useState('');
+  const [selectedField, setSelectedField] = useState('all');
   const { isLoading, data: featureData } = useGetFeatureQuery(undefined, {
     refetchOnMountOrArgChange: true,
     pollingInterval: 40000,
@@ -16,31 +15,29 @@ export default function FeaturedSection() {
   if (isLoading) {
     return <span className="loading loading-spinner loading-lg"></span>;
   }
-
   let features = featureData.data;
 
-  if (features) {
-    const options = {
-      keys: ["title", "owner", "localAuthority"],
-      threshold: 0.4,
-    };
-    const fuse = new Fuse(features, options);
-    const searchResults = fuse.search(searchText);
+  const newFilteredList = features.filter((item: any) => {
+    if (selectedField === item.title) {
+      return item;
+    } else if (selectedField === 'all') {
+      return item;
+    }
+  });
 
-    features =
-      searchResults.length > 0
-        ? searchResults.map((result) => result.item)
-        : features;
-  }
+  const filterableData = (payload: any) => {
+    setSelectedField(payload);
+  };
 
   return (
     <div className="bg-[#F8F8FF]">
       <SearchAndFilter
-        selectedGenre={selectedGenre}
-        setSelectedGenre={setSelectedGenre}
-        searchText={searchText}
         setSearchText={setSearchText}
+        searchText={searchText}
+        features={features}
+        filterableData={filterableData}
       />
+
       <div className="max-w-[1200px] py-10 mx-auto">
         <h3 className="text-2xl mb-10 text-teal-600 my-4 ">
           <span className="border-b-2 border-teal-400 ">
@@ -48,23 +45,29 @@ export default function FeaturedSection() {
           </span>
         </h3>
         <div className="grid gap-4 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-4 xl:grid-cols-4 ">
-          {features?.map((feature: any) => (
-            <Link key={feature._id} to={`/details/${feature._id}`}>
-              <div className="card hover:border hover:border-teal-600  shadow-xl rounded min-h-full">
-                <img
-                  src={feature.img[0]}
-                  className="rounded-md w-full max-h-44"
-                  alt=""
-                />
-                <div className="flex justify-around my-6 items-center">
-                  <h2 className="text-[#23527C] text-md font-serif text-lg">
-                    {feature.title}
-                  </h2>
-                  <img src={feature.logo} alt="" className="h-12 w-24" />
+          {newFilteredList
+            ?.filter((item: any) => {
+              return searchText.toLowerCase() === ''
+                ? item
+                : item.title.toLowerCase().includes(searchText);
+            })
+            .map((feature: any) => (
+              <Link key={feature._id} to={`/details/${feature._id}`}>
+                <div className="card hover:border hover:border-teal-600  shadow-xl rounded min-h-full">
+                  <img
+                    src={feature.img[0]}
+                    className="rounded-md w-full max-h-44"
+                    alt=""
+                  />
+                  <div className="flex justify-around my-6 items-center">
+                    <h2 className="text-[#23527C] text-md font-serif text-lg">
+                      {feature.title}
+                    </h2>
+                    <img src={feature.logo} alt="" className="h-12 w-24" />
+                  </div>
                 </div>
-              </div>
-            </Link>
-          ))}
+              </Link>
+            ))}
           {features === null && <div>No data available</div>}
         </div>
       </div>
